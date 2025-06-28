017201ab0adbf8340c4fa6e0550741377390ca2d
rgimad <33692565+rgimad@users.noreply.github.com>
2025-06-28 23:30:56 +0300

    Add calling make_image.py script from the main meson.build

--- make_image.py
+++ make_image.py
@@ -8,22 +8,23 @@ def run_cmd_pretty(cmd):
 
 
 # to test, in the root of the project run
-# python3 make_image.py --srctree . --builddir build_en --targetimg neolibrios.img
+# python3 make_image.py --srcdir . --builddir build_en --targetimg neolibrios.img
 
 def main():
     parser = argparse.ArgumentParser(description='Process args')
 
-    parser.add_argument('--srctree', required=True, help='Path to source tree')
+    parser.add_argument('--srcdir', required=True, help='Path to sources dir')
     parser.add_argument('--builddir', required=True, help='Path to build dir')
     parser.add_argument('--targetimg', required=True, help='Target image path')
+    parser.add_argument('--lang', required=True, help='Language of target image')
 
 
     args = parser.parse_args()
 
-    # print(f"Source tree: {args.srctree}")
+    # print(f"Sources directory: {args.srcdir}")
     # print(f"Build directory: {args.builddir}")
     # print(f"Target image: {args.targetimg}")
-    
+
     # create empty 1.44M file
     run_cmd_pretty(f"dd status=none if=/dev/zero of={args.targetimg} count=2880 bs=512")
 
